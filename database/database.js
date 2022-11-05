30b7c3a79061302bf2789c829731e1657f3efea7
RafaelGusto <rafael.gustojunior@gmail.com>
2022-11-05 13:54:02 -0300

    iniciando página principal

--- database/database.js
+++ database/database.js
@@ -4,6 +4,7 @@ const sequelize = require('sequelize');
 const connection = new Sequelize('bloguinho','root','',{
     host: 'localhost',
     dialect: 'mysql',
+    timezone: '-03:00',
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -31,7 +31,27 @@ connection
     })
 
 app.get("/", (req, res) => {
-    res.render("index")
+    Article.findAll().then(articles => {
+        res.render("index", {articles: articles});
+    })
+});
+
+app.get('/:slug', (req, res) => {
+    var slug = req.params.slug;
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            res.render('article', {article: article});
+        } else {
+            res.redirect('/');
+        }
+    }).catch( err => {
+        res.redirect('/');
+    })
 })
 
 app.listen(8080, () => {
