c3202360fb358f85cc4b0e2af9070faa6ef25d92
RafaelGusto <rafael.gustojunior@gmail.com>
2022-11-06 16:57:14 -0300

    atualizando paginas

--- index.js
+++ index.js
@@ -31,8 +31,14 @@ connection
     })
 
 app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-        res.render("index", {articles: articles});
+    Article.findAll({
+        order: [
+            ['id','DESC']
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        })
     })
 });
 
@@ -45,14 +51,38 @@ app.get('/:slug', (req, res) => {
         }
     }).then(article => {
         if (article != undefined) {
-            res.render('article', {article: article});
+            Category.findAll().then(categories => {
+                res.render("article", {articles: article, categories: categories});
+            })
         } else {
             res.redirect('/');
         }
     }).catch( err => {
         res.redirect('/');
     })
-})
+});
+
+app.get('/category/:slug', (req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if (category != undefined) {
+            
+            Category.findAll().then(categories => {
+                res.render('index', {articles: category.articles, categories: categories});
+            })
+
+        } else {
+            res.redirect('/');
+        }
+    }).catch( err => {
+        res.redirect('/');
+    })
+});
 
 app.listen(8080, () => {
     console.log("O servidor está rodando")
