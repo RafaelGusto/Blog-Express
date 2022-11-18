60b909fd100e3498852191b1ad7e660ea522833f
RafaelGusto <rafael.gustojunior@gmail.com>
2022-11-17 22:05:40 -0300

    projeto finalizado com sessões

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const session = require('express-session');
 const connection = require('./database/database');
 
 
@@ -14,6 +15,16 @@ const User = require('./users/User');
 
 // view engine
 app.set('view engine','ejs');
+
+// Redis
+
+
+
+// Sessions
+
+app.use(session({
+    secret: "qualquercoisa", cookie: { maxAge: 300000} // cookie que aponta para a sessão ativa no servidor
+}))
 
 // static
 app.use(express.static('public'));
@@ -35,6 +46,30 @@ connection
     }).catch((error) => {
         console.log(error)
     })
+
+
+
+
+app.get('/session', (req, res) => {
+    req.session.treinamento = "Formação Node.js"
+    req.session.ano = 2019
+    req.session.email = "rafael@udemy.com"
+    req.session.user = {
+        username: 'rafael',
+        email: "email@email.com",
+        id: 10
+    }
+    res.send('Sessão gerada');
+});
+
+app.get('/leitura', (req, res) => {
+    res.json({
+        treinamento: req.session.treinamento,
+        ano: req.session.ano,
+        email: req.session.email,
+        user: req.session.user
+    })
+});
 
 app.get("/", (req, res) => {
     Article.findAll({
